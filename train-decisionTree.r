5042178c8fac698db9571759c25be4b1e07489d2
rstinejr <bob@waltonstine.net>
2016-02-13 11:41:44 -0500

    define response buckets r1 thru r8 for Response.

--- train-decisionTree.r
+++ train-decisionTree.r
@@ -13,3 +13,11 @@ train     <- applicants[trainInd,]
 test      <- applicants[-trainInd,]
 cat("\nTraining set extracted, mean ID of training set: ", mean(train$Id), "\n")
 cat("                        mean ID of test set    : ",   mean(test$Id),  "\n")
+r1 <- applicants[applicants$Response == 1,]
+r2 <- applicants[applicants$Response == 2,]
+r3 <- applicants[applicants$Response == 3,]
+r4 <- applicants[applicants$Response == 4,]
+r5 <- applicants[applicants$Response == 5,]
+r6 <- applicants[applicants$Response == 6,]
+r6 <- applicants[applicants$Response == 7,]
+r8 <- applicants[applicants$Response == 8,]
