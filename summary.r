6a30bd44cdbe7903fbc6d3f3e2f053928f71e3de
rstinejr <bob@waltonstine.net>
2016-02-13 11:52:27 -0500

    Chunk continuous vaule Product_Info_4 into 5 categories.

--- summary.r
+++ summary.r
@@ -1,3 +1,4 @@
 cat("Summary of applicant data")
 applicants  <- read.csv("applicants.csv",head=TRUE,sep=",")
+applicants$Product_Info_4_cat <- cut(applicants$Product_Info_4, 5)
 summary(applicants)
--- train-decisionTree.r
+++ train-decisionTree.r
@@ -6,6 +6,8 @@ applicants  <- read.csv("applicants.csv",head=TRUE,sep=",")
 totRows <- nrow(applicants)
 cat("Applicant data ingested, rows: ", totRows, "\n")
 
+applicants$Product_Info_4_cat <- cut(applicants$Product_Info_4, 5)
+
 set.seed(2718)
 trainSize <- totRows / 2
 trainInd  <- sample(seq_len(totRows), size = trainSize)
@@ -13,6 +15,7 @@ train     <- applicants[trainInd,]
 test      <- applicants[-trainInd,]
 cat("\nTraining set extracted, mean ID of training set: ", mean(train$Id), "\n")
 cat("                        mean ID of test set    : ",   mean(test$Id),  "\n")
+
 r1 <- applicants[applicants$Response == 1,]
 r2 <- applicants[applicants$Response == 2,]
 r3 <- applicants[applicants$Response == 3,]
@@ -21,3 +24,4 @@ r5 <- applicants[applicants$Response == 5,]
 r6 <- applicants[applicants$Response == 6,]
 r6 <- applicants[applicants$Response == 7,]
 r8 <- applicants[applicants$Response == 8,]
+
