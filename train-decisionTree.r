9aa779d933c588bf1e838d3e69a0a3ba6d8be5ee
rstinejr <bob@waltonstine.net>
2016-02-13 13:50:09 -0500

    Reset nominal policy columns to be as.factor.w

--- train-decisionTree.r
+++ train-decisionTree.r
@@ -1,3 +1,4 @@
+library(e1071)
 library(plyr)
 library(randomForest)
 library(rpart)
@@ -6,13 +7,12 @@ cat("Start training session for decision trees.\n")
 applicants  <- read.csv("applicants.csv",head=TRUE,sep=",")
 totRows <- nrow(applicants)
 cat("Applicant data ingested, rows: ", totRows, "\n")
-
-applicants$Product_Info_4_cat <- cut(applicants$Product_Info_4, 5)
-
-applicants$Ins_Age_cat <- cut(applicants$Ins_Age, 8)
-applicants$Ht_cat      <- cut(applicants$Ht, 4)
-applicants$Wt_cat      <- cut(applicants$Wt, 8)
-applicants$BMI_cat     <- cut(applicants$BMI, 4)
+applicants$Product_Info_1 <- as.factor(applicants$Product_Info_1)
+applicants$Product_Info_2 <- as.factor(applicants$Product_Info_2)
+applicants$Product_Info_3 <- as.factor(applicants$Product_Info_3)
+applicants$Product_Info_5 <- as.factor(applicants$Product_Info_5)
+applicants$Product_Info_6 <- as.factor(applicants$Product_Info_6)
+applicants$Product_Info_7 <- as.factor(applicants$Product_Info_7)
 
 set.seed(2718)
 trainSize <- totRows / 2
@@ -22,16 +22,8 @@ test      <- applicants[-trainInd,]
 cat("\nTraining set extracted, mean ID of training set: ", mean(train$Id), "\n")
 cat("                        mean ID of test set    : ",   mean(test$Id),  "\n")
 
-r1 <- applicants[applicants$Response == 1,]
-r2 <- applicants[applicants$Response == 2,]
-r3 <- applicants[applicants$Response == 3,]
-r4 <- applicants[applicants$Response == 4,]
-r5 <- applicants[applicants$Response == 5,]
-r6 <- applicants[applicants$Response == 6,]
-r6 <- applicants[applicants$Response == 7,]
-r8 <- applicants[applicants$Response == 8,]
 
-fol1 <- formula(Response ~ Product_Info_1 + Product_Info_2 + Product_Info_3 + Product_Info_4_cat 
+fol1 <- formula(Response ~ Product_Info_1 + Product_Info_2 + Product_Info_3 + Product_Info_4
                           + Product_Info_5 + Product_Info_6 + Product_Info_7)
 model1 <- rpart(fol1, method="class", data=train)
 cat("\nDecision tree for product type:\n")
@@ -41,9 +33,3 @@ fol2   <- formula(Response ~ Ins_Age, Ht, Wt, BMI)
 model2 <- rpart(fol2, method="class", data=train)
 cat("\nDecision tree for age, weight:\n")
 model2
-
-mod_rf = randomForest(fol2, train)
-cat("\n\nRandom forest, age and weight:\n")
-mod_rf
-cat("\nimportance:\n")
-importance(mod_rf)
